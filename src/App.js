407d8985b6f20e84a98cb0ac11625776a656be7f
javisolercanto <jsolercanto11@gmail.com>
2021-01-24 18:29:51 +0100

    surge deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Uploaded page by <code>Javier Soler Cantó</code>
         </p>
         <a
           className="App-link"
